cc169701e41bea588e020ac9fc24c4ba9fbc6a35
Shubham Patrick <ahthneeuhl@gmail.com>
2023-06-19 21:29:40 +0530

    feat: write GraphQL query, generate types using GraphQL Codegen and Query Users data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,15 @@
+import { useQuery } from "urql";
 import UserDisplay from "./components/UserDisplay";
-import { User } from "./types";
+import { GetUsersDocument } from "./graphql/generated";
 
 const App = () => {
-  const users: User[] = [
-    {
-      name: "Prisma Fan",
-      messages: [
-        { body: "Prisma rocks!!" },
-        { body: "Did I mention I love Prisma?" },
-      ],
-    },
-  ];
+  const [results] = useQuery({
+    query: GetUsersDocument,
+  });
 
   return (
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
-      {users.map((user, idx) => (
+      {results.data?.users.map((user, idx) => (
         <UserDisplay user={user} key={idx} />
       ))}
     </div>
