cd2f3de8526c1a919fd86d25c1ccee28683c64ce
Shubham Patrick <ahthneeuhl@gmail.com>
2023-06-19 18:14:42 +0530

    config: graphql server, schema builder, date scalar types and prisma client instance setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hey there! 👋");
+import { createSchema, createYoga } from "graphql-yoga";
+import { createServer } from "http";
+
+const PORT = Number(process.env.PORT) || 8080;
+
+const schema = createSchema({
+  typeDefs: /* GraphQL */ `
+    type Query {
+      hello: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => "world",
+    },
+  },
+});
+
+const yoga = createYoga({ schema });
+
+const server = createServer(yoga);
+
+server.listen(PORT, () => {
+  console.log(`🚀 GraphQL Server ready at http://localhost:${PORT}/graphql`);
+});
